fca7cb90b711a21fc5d0b99e8a342c23aceed89e
adiy55 <adiyafe55@gmail.com>
2022-03-22 18:37:40 +0200

    Change uint to int

--- Notebook.cpp
+++ Notebook.cpp
@@ -2,22 +2,20 @@
 
 using std::string;
 
-typedef unsigned int uint;
-
 namespace ariel {
 
-    void Notebook::write(uint page, uint row, uint column, Direction direction, const string &str) {
+    void Notebook::write(int page, int row, int column, Direction direction, const string &str) {
         _notebook.find(page);
     }
 
-    string Notebook::read(uint page, uint row, uint column, Direction direction, uint str_len) {
+    string Notebook::read(int page, int row, int column, Direction direction, int str_len) {
         _notebook.find(page);
         return "";
     }
 
-    void Notebook::erase(uint page, uint row, uint column, Direction direction, uint str_len) {}
+    void Notebook::erase(int page, int row, int column, Direction direction, int str_len) {}
 
-    void Notebook::show(uint page) {
+    void Notebook::show(int page) {
         _notebook.find(page);
     }
 
--- Notebook.hpp
+++ Notebook.hpp
@@ -11,19 +11,19 @@
 namespace ariel {
 
     class Notebook {
-        std::unordered_map<unsigned int, std::map<unsigned int, std::array<char, 100>>> _notebook;
+
+    private:
+        std::unordered_map<int, std::map<int, std::array<char, 100>>> _notebook;
 
     public:
 
-        void
-        write(unsigned int page, unsigned int row, unsigned int column, Direction direction, const std::string &str);
+        void write(int page, int row, int column, Direction direction, const std::string &str);
 
-        std::string
-        read(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int str_len);
+        std::string read(int page, int row, int column, Direction direction, int str_len);
 
-        void erase(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int str_len);
+        void erase(int page, int row, int column, Direction direction, int str_len);
 
-        void show(unsigned int page);
+        void show(int page);
 
     };
 
