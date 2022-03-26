193ca2fc2e1c8afb8418c9c16e4e91c8ee85c098
adiy55 <adiyafe55@gmail.com>
2022-03-26 17:13:38 +0300

    Add documentation to Page

--- sources/Page.cpp
+++ sources/Page.cpp
@@ -4,8 +4,13 @@ using std::string;
 using std::vector;
 using ariel::Direction;
 
-typedef unsigned int uint;
+typedef unsigned int uint; // assign a shorter name to an existing datatype (more readable code)
 
+/**
+ * If row does not exists- initializes a vector of underscores.
+ * @param row number of the row
+ * @return reference to the row
+ */
 vector<char> &Page::getRow(int row) {
     if (!_rows.contains(row)) { // todo: change to count?
         _rows[row] = vector<char>(ROW_LENGTH, UNDERSCORE);
@@ -13,23 +18,33 @@ vector<char> &Page::getRow(int row) {
     return _rows[row];
 }
 
+/**
+ * Get a specific part of the text on the page.
+ * @param str_len number of chars to get
+ * @return string of the requested length
+ */
 string Page::getSection(int row, int column, Direction direction, int str_len) {
     string section;
     if (direction == Direction::Horizontal) {
-        auto &line = this->getRow(row);
+        vector<char> &line = this->getRow(row);
         for (int i = column; i < (str_len + column); ++i) {
             section.push_back(line.at(uint(i)));
         }
     } else if (direction == Direction::Vertical) {
         int length = str_len + row;
         for (int i = row; i < length; ++i) {
-            auto &line = this->getRow(i);
+            vector<char> &line = this->getRow(i);
             section.push_back(line.at(uint(column)));
         }
     }
     return section;
 }
 
+/**
+ * Write on a specific part of the page.
+ * Throws exception if trying to write over written or erased text.
+ * @param str text to write
+ */
 void Page::write(int row, int column, Direction direction, const string &str) {
     if (direction == Direction::Horizontal) {
         vector<char> &line = this->getRow(row);
@@ -50,29 +65,54 @@ void Page::write(int row, int column, Direction direction, const string &str) {
     }
 }
 
+/**
+ * Erases a specific part on the page.
+ * @param str_len number of chars to erase
+ */
+/*
+ * This method calls the write method (implemented above) to mark over the section.
+ */
 void Page::erase(int row, int column, ariel::Direction direction, int str_len) {
     string tilda_str;
     tilda_str.append(uint(str_len), TILDA);
     this->write(row, column, direction, tilda_str);
 }
 
-void Page::printPage() {
+/**
+ * Prints the page. Prints a line of underscores if the line is empty.
+ */
+/*
+ * The loop uses structured bindings (since C++17).
+ * This allows to easily unpack key and value references in the map.
+ * auto keyword detects the variable type: "The referenced type for the i-th identifier is std::tuple_element<i, E>::type"
+ * Additional information: https://en.cppreference.com/w/cpp/language/structured_binding
+ */
+void Page::printPage() const {
     int curr_row_num = 0;
     string str;
     str.append(ROW_LENGTH, UNDERSCORE);
-    for (auto &[key, value]: _rows) {
+    for (auto &[key, value]: _rows) { //
         for (; curr_row_num < key; ++curr_row_num) {
             std::cout << str << '\n';
         }
-        Page::printRow(value);
+        Page::printRow(value); // static helper function
     }
 }
 
-void Page::printRow(vector<char> &row) {
+/**
+ * Prints a given row.
+ * @param row const reference (type: vector<char>)
+ */
+void Page::printRow(const vector<char> &row) {
     string curr_row = string(row.begin(), row.end());
     std::cout << curr_row << '\n';
 }
 
+/**
+ * Checks if the place next character to write is valid.
+ * @param curr_char char to overwrite
+ * @param new_char char to be written
+ */
 void Page::checkValidForWrite(char curr_char, char new_char) {
     if (curr_char != UNDERSCORE && new_char != TILDA) {
         throw std::invalid_argument("Can not write over existing or erased text!");
--- sources/Page.hpp
+++ sources/Page.hpp
@@ -7,21 +7,34 @@
 #include <string>
 #include "Direction.hpp"
 
+/*
+ * Defined macros for vital values in the assignment.
+ * Advantages of this approach:
+ * Global (not scope controlled) and does not require memory allocation (pre-processor runs before the compiler).
+ */
 #define ROW_LENGTH 100
 #define TILDA '~'
 #define UNDERSCORE '_'
 
+/*
+ * OOP approach -> Page object allows easier functionality in Notebook class.
+ */
 class Page {
 
 private:
 
-    std::map<int, std::vector<char>> _rows; // todo: check if map keeps order by ascending key
+    /*
+     * Map: unique keys, typically a red-black tree,
+     * sorted by increasing order of keys - this is handy for printing a whole page
+     * Additional information: https://en.cppreference.com/w/cpp/container/map
+     */
+    std::map<int, std::vector<char>> _rows;
 
     std::vector<char> &getRow(int row);
 
     static void checkValidForWrite(char, char);
 
-    static void printRow(std::vector<char> &row);
+    static void printRow(const std::vector<char> &row);
 
 public:
 
@@ -31,7 +44,7 @@ public:
 
     void erase(int row, int column, ariel::Direction direction, int str_len);
 
-    void printPage();
+    void printPage() const;
 
 };
 
